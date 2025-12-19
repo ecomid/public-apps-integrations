5844770891139e785f4ea4808d227857107f4502
Patrick Rengifo <patrick.samuel@ecomid.com>
2025-12-19 10:08:07 +0100

    Removed overlay package. Updated min iOS

--- Package.swift
+++ Package.swift
@@ -1,16 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "EComIDIntegrationsPublic",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "EComIDIntegrationsOverlay",
-            targets: ["EComIDIntegrationsOverlay"]),
         .library(
             name: "EComIDIntegrationsWidgets",
             targets: ["EComIDIntegrationsWidgets"]),
@@ -18,10 +15,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(
-            name: "EComIDIntegrationsOverlay",
-            path: "./Sources/EComIDIntegrationsOverlay.xcframework"
-        ),
         .binaryTarget(
             name: "EComIDIntegrationsWidgets",
             path: "./Sources/EComIDIntegrationsWidgets.xcframework"
