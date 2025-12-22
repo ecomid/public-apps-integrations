066dffb535a3f66ca61c4cedb5890ebe41f6e1b9
Patrick Rengifo <patrick.samuel@ecomid.com>
2025-12-22 11:20:01 +0100

    Renamed package (#2)

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "EComIDIntegrationsWidgets",
-            targets: ["EComIDIntegrationsWidgets"]),
+            name: "EComIDWidgets",
+            targets: ["EComIDWidgets"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "EComIDIntegrationsWidgets",
-            path: "./Sources/EComIDIntegrationsWidgets.xcframework"
+            name: "EComIDWidgets",
+            path: "./Sources/EComIDWidgets.xcframework"
         )
     ]
 )
